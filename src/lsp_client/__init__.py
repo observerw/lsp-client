3147b612da4023febca8cf46ff288f1c3c6fe796
observerw <wozluohd@gmail.com>
2025-07-13 11:06:57 +0800

    doc: update README

--- src/lsp_client/__init__.py
+++ src/lsp_client/__init__.py
@@ -3,18 +3,17 @@ from __future__ import annotations
 import lsprotocol.types as lsp_type
 
 from . import capability as lsp_cap
+from . import servers as lsp_servers
 from .client import LSPClientBase
 from .server import LSPServerInfo
 from .types import Position, Range
-from .utils.path import AbsPath, RelPath
 
 __all__ = [
-    "AbsPath",
     "LSPClientBase",
     "LSPServerInfo",
     "Position",
     "Range",
-    "RelPath",
     "lsp_cap",
+    "lsp_servers",
     "lsp_type",
 ]
