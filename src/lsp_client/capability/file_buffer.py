4034a20ab5cbd0662c8ba1d21d7284c02a2a6118
observerw <wozluohd@gmail.com>
2025-08-02 11:20:29 +0800

    fix: use uri to identify file buffer

--- src/lsp_client/capability/file_buffer.py
+++ src/lsp_client/capability/file_buffer.py
@@ -2,20 +2,19 @@ from __future__ import annotations
 
 from collections import Counter
 from collections.abc import Iterable, Sequence
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from functools import cached_property
-
-from lsp_client.utils.path import AbsPath
+from pathlib import Path
 
 
 @dataclass(frozen=True)
 class LSPFileBufferItem:
-    file_path: AbsPath
+    file_uri: str
     file_content: bytes
 
     @cached_property
-    def uri(self) -> str:
-        return self.file_path.as_uri()
+    def file_path(self) -> Path:
+        return Path.from_uri(self.file_uri)
 
     @cached_property
     def content(self) -> str:
@@ -27,46 +26,41 @@ class LSPFileBufferItem:
         return 0
 
 
+@dataclass
 class LSPFileBuffer:
-    type PosixPath = str
+    _lookup: dict[str, LSPFileBufferItem] = field(default_factory=dict)
+    _ref_count: Counter[str] = field(default_factory=Counter)
 
-    _lookup: dict[PosixPath, LSPFileBufferItem]
-    _ref_count: Counter[PosixPath]
-
-    def __init__(self) -> None:
-        self._lookup = {}
-        self._ref_count = Counter()
-
-    def open(self, file_paths: Iterable[AbsPath]) -> Sequence[LSPFileBufferItem]:
+    def open(self, file_uris: Iterable[str]) -> Sequence[LSPFileBufferItem]:
         """Open files and save to buffer. Only return newly opened files."""
 
-        self._ref_count.update(file_path.as_posix() for file_path in file_paths)
+        self._ref_count.update(file_uris)
 
         items: list[LSPFileBufferItem] = []
-        for file_path in file_paths:
-            if (file_path_posix := file_path.as_posix()) in self._lookup:
+        for uri in file_uris:
+            if uri in self._lookup:
                 continue
 
-            item = self._lookup[file_path_posix] = LSPFileBufferItem(
-                file_path=file_path, file_content=file_path.read_bytes()
+            item = self._lookup[uri] = LSPFileBufferItem(
+                file_uri=uri,
+                file_content=Path.from_uri(uri).read_bytes(),
             )
             items.append(item)
 
         return items
 
-    def close(self, file_paths: Iterable[AbsPath]) -> Sequence[AbsPath]:
+    def close(self, file_uris: Iterable[str]) -> Sequence[LSPFileBufferItem]:
         """
         Close the files. Return paths of files that are really closed (ref count reaches 0).
         """
 
-        closed_files: list[AbsPath] = []
+        self._ref_count.subtract(file_uris)
 
-        self._ref_count.subtract(path.as_posix() for path in file_paths)
-        for path, ref_count in self._ref_count.items():
+        closed_items: list[LSPFileBufferItem] = []
+        for uri, ref_count in self._ref_count.items():
             if ref_count > 0:
                 continue
+            if item := self._lookup.pop(uri, None):
+                closed_items.append(item)
 
-            closed_files.append(AbsPath(path))
-            self._lookup.pop(path, None)
-
-        return closed_files
+        return closed_items
