f2b1d72c032b66191eccb9907e522e95332678ac
observerw <wozluohd@gmail.com>
2025-08-29 14:18:18 +0800

    chore: disable loguru logging by default

--- src/lsp_client/__init__.py
+++ src/lsp_client/__init__.py
@@ -8,6 +8,9 @@ from .client.base import LSPClient
 from .server.base import LSPServer
 from .types import Position, Range
 
+# https://loguru.readthedocs.io/en/stable/overview.html#suitable-for-scripts-and-libraries
+logger.disable("lsp_client")
+
 __all__ = [
     "LSPClient",
     "LSPServer",
