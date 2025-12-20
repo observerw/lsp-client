9c71871c691aa795a1c122a26d158baadca2c7cb
observerw <wozluohd@gmail.com>
2025-12-21 01:04:20 +0800

    revert: remove packages/ directory and related changes

--- packages/lsp-client-js/src/utils/uri.ts
+++
@@ -1,9 +0,0 @@
-import { pathToFileURL, fileURLToPath } from 'url';
-
-export function toURI(path: string): string {
-  return pathToFileURL(path).toString();
-}
-
-export function fromURI(uri: string): string {
-  return fileURLToPath(uri);
-}
