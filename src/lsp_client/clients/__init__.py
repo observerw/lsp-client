bfee62292441cd86a9a724d13c100b802530a3ac
observerw <wozluohd@gmail.com>
2025-12-21 13:03:43 +0800

    fix: remove unused export

--- src/lsp_client/clients/__init__.py
+++ src/lsp_client/clients/__init__.py
@@ -1,23 +1,17 @@
 from __future__ import annotations
 
-from .deno import DenoClient
 from .pyrefly import PyreflyClient
 from .pyright import PyrightClient
 from .rust_analyzer import RustAnalyzerClient
-from .ty import TyClient
 from .typescript import TypescriptClient
 
 PythonClient = PyrightClient
 RustClient = RustAnalyzerClient
 TypeScriptClient = TypescriptClient
-DenoLSPClient = DenoClient
-TyLSPClient = TyClient
 
 __all__ = [
-    "DenoLSPClient",
     "PyreflyClient",
     "PythonClient",
     "RustClient",
-    "TyLSPClient",
     "TypeScriptClient",
 ]
