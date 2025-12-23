c4c103fbca34e7e19da96e089066b94e3d850e30
observerw <wozluohd@gmail.com>
2025-12-23 19:29:43 +0800

    docs: remove redundant comments from capability tests

--- tests/test_inspect_clients.py
+++ tests/test_inspect_clients.py
@@ -32,18 +32,12 @@ async def test_client_capabilities_match_container(client_cls):
 
     mismatches = []
     async for result in inspect_capabilities(server, client_cls):
-        # We only care about capabilities that the client CLAIMS to support but the server doesn't,
-        # or vice versa (though usually we care more about the client expecting something the server doesn't provide)
         if result.client != result.server:
             mismatches.append(
                 f"{result.capability}: client={result.client}, server={result.server}"
             )
 
     if mismatches:
-        # For now, let's just log them or fail if we want strict matching.
-        # The user asked to "detect", so failing on mismatch is a good way to ensure they stay in sync.
-        # However, some servers might legitimately not support some things that we've implemented in the client (maybe for newer versions).
-        # But usually they should match if we are targeting a specific version in the container.
         pytest.fail(
             f"Capability mismatch for {client_cls.__name__}:\n" + "\n".join(mismatches)
         )
